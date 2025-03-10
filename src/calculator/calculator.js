4575c068cbd5e83d20a533fa59a09245beca5565
AlkayidAli <alialkayed2014@gmail.com>
2025-03-10 01:24:11 +0100

    Milestone#3: Calculator App | Layout

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -2,8 +2,18 @@ import "./calculator.scss";
 import { loadComponent } from "../utils/loadComponents";
 import headerHtml from "../components/common/header/header.html";
 import footerHtml from "../components/common/footer/footer.html";
+import "../components/common/header/header.scss";
+import "../components/common/footer/footer.scss";
+import "../components/common/header/header.js";
+import arrowIcon from "../images/icons/d-arrow.png";
 
 document.addEventListener("DOMContentLoaded", () => {
   loadComponent("header", headerHtml);
   loadComponent("footer", footerHtml);
+
+  // Set arrow image src
+  const historyToggle = document.getElementById("history-toggle");
+  historyToggle.addEventListener("click", () => {
+    document.querySelector(".history-container").classList.toggle("active");
+  });
 });
